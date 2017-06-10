1eba90f7c21013dc443fdfc189a5e5de7b926171
linzhun <linzhun7@gmail.com>
2017-06-10 09:24:41 +0800

    customize the admin site

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+
+admin.site.register(Post, PostAdmin)
