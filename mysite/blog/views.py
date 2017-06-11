d6880e8e61fc2ed1ba5f1889a74e44e54210e78e
linzhun <linzhun7@gmail.com>
2017-06-11 09:21:06 +0800

    define the views as class methods

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
 #def post_list(request):
@@ -32,3 +33,10 @@ def post_list(request):
                   'blog/post/list.html',
                   {'page': page,
                    'posts':posts})
+                   
+# This class-based view is analogous to the previous post_list view.                   
+class PostListView(ListView):
+    queryset = Post.published.all() #Use a specific QuerySet 【attribute】 instead of retrieving all objects
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'                  
