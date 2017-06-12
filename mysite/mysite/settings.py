43de84c35ed5b0969a6d64ff757a58d7fa43c864
linzhun <linzhun7@gmail.com>
2017-06-12 13:41:49 +0800

    build a comment model and to generate a new migration

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 #EMAIL
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'lianzaime@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'qxlz20000'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
