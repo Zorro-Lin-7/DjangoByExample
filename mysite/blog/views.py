c83ed52af77f92611e97227bcf7bf7b350243ab9
linzhun <linzhun7@gmail.com>
2017-06-14 11:03:51 +0800

    Retrieving posts by similarity:views:uncomment some lines

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
-
+from django.db.models import Count
 # Create your views here.
 #def post_list(request):
 #    posts = Post.published.all()
@@ -33,10 +33,10 @@ def post_detail(request, year, month, day, post):
         comment_form = CommentForm()
         
     # List of similar posts
-    #post_tags_ids = post.tags.values_list('id',falt=True)
-    #similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
-    #similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags',
-    #                                                                         '-publish')[:4]
+    post_tags_ids = post.tags.values_list('id',falt=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags',
+                                                                             '-publish')[:4]
                                                                                                         
     return render(request,'blog/post/detail.html',{'post':post,
                                                    'comments': comments,
