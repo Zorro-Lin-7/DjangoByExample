ded1c2dd73b32903aeef97716f21de0593dcfc34
linzhun <linzhun7@gmail.com>
2017-06-17 08:01:05 +0800

    to activate both the sites and the sitemap apps

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#Define a new setting for the site ID
+SITE_ID = 1
 
 # Application definition
 
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
