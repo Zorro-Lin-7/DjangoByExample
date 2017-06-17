442e9da7ab5a0008a2b0561ba9f581f76d182e44
linzhun <linzhun7@gmail.com>
2017-06-17 16:25:50 +0800

    Creating feeds for your blog posts:create a feeds.py file;instantiate the feed in a new url pattern

---
+++ mysite/blog/feeds.py
@@ -0,0 +1,19 @@
+from django.contrib.syndication.views import Feed
+from django.tamplate.defaultfilters import truncatewords
+from .models import Post
+
+class LatestPostsFeed(Feed):
+    #RSS elements
+    title = "My blog"
+    link = '/blog/'
+    description = 'New posts of my blog.'
+    
+    # retrieves the objects to be included in the feed
+    def items(self):  			
+        return Post.published.all()[:5]
+        
+    def item_title(self, item):
+        return item.title
+        
+    def item_description(self, item):
+        return truncatewords(item.body, 30)
