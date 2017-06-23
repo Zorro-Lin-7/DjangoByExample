00b595efd77b7bbd17c8402c11f94769584fe66d
linzhun <linzhun7@gmail.com>
2017-06-24 06:54:33 +0800

    debug:spelling errors

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .form import LoginForm
+from .forms import LoginForm
 
 
 # Create your views here.
