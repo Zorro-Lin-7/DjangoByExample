842a178292487add5ed27ea904dd60667978b5e4
linzhun <linzhun7@gmail.com>
2017-06-24 09:10:02 +0800

    Add the URL patterns and the templates for log in and log out views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -4,5 +4,16 @@ from . import views
 
 urlpatterns = [
     # post view
-    url(r'^login/$', views.user_login, name='login'),
+    #url(r'^login/$', views.user_login, name='login'),
+    
+    #login/ logout urls
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        name='login'),
+    url(r'^logout/$',
+        'django.contrib.auth.views.logout',
+        name='logout'),
+    url(r'^logout-then-login/$',
+        'django.contrib.auth.views.logout_then_login',
+        name='logout_then_login'),
 ]
