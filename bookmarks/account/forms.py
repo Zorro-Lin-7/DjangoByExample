e47f8013a83a6262db6f3949924ee89d272e29d9
linzhun <linzhun7@gmail.com>
2017-06-26 08:18:02 +0800

    debug:spelling errors and indentation

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -7,7 +7,7 @@ class LoginForm(forms.Form):
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
     
     class Meta:
         model = User
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -39,11 +39,12 @@ def dashboard(request):
 def register(request):
     if request.method == "POST":
         user_form = UserRegistrationForm(request.POST)
+        
         if user_form.is_valid():
             new_user = user_form.save(commit=False) #Create a new user object but avoid saving it yet
             new_user.set_password(user_form.cleaned_data['password']) #Set the chosen password
             new_user.save()  # Save the User object
-            return render(request, 'account/registration_done.html',{'new_user': new_user})
-        else:
-            user_form = UserRegistrationForm()
-        return render(request,'account/register.html',{'user_form': user_form})    
+            return render(request, 'account/register_done.html',{'new_user': new_user})
+    else:                     # 此处犯过错，多了个缩进
+        user_form = UserRegistrationForm()
+    return render(request,'account/register.html',{'user_form': user_form})    
